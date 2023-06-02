a5cb455ca967167cf89532ff7e4e4a010145e0d5
Lyle Arcinas <60361950+lylearcinas@users.noreply.github.com>
2023-06-02 17:48:45 -0500

    Added getters and setters to recipe

--- app/src/main/java/comp3350/g3/tasteBud/data/RecipePersistence.java
+++ app/src/main/java/comp3350/g3/tasteBud/data/RecipePersistence.java
@@ -1,5 +1,16 @@
 package comp3350.g3.tasteBud.data;
 
+import java.util.*;
+import comp3350.g3.tasteBud.object.Recipe;
+
 public class RecipePersistence {
-    
+    private List<Recipe> storedRecipes;
+
+    public RecipePersistence(List<Recipe> storedRecipes) {
+        this.storedRecipes = storedRecipes;
+    }
+
+    public void add(Recipe newRecipe) {
+        this.storedRecipes.add(newRecipe);
+    }
 }
