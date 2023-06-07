fdac353bddbf103234ea1b7ec6b2428d3e9ed7a6
ThreshvsGaming <135454181+ThreshvsGaming@users.noreply.github.com>
2023-06-06 19:38:17 -0500

    Search Recipe Function
    Shows a search bar display.
    Provides the Search Logic.

--- app/src/main/java/comp3350/g3/tasteBud/logic/SearchProcessor.java
+++ app/src/main/java/comp3350/g3/tasteBud/logic/SearchProcessor.java
@@ -1,5 +1,52 @@
 package comp3350.g3.tasteBud.logic;
 
+import java.util.*;
+import java.util.regex.*;
+
+import comp3350.g3.tasteBud.data.RecipeStub;
+import comp3350.g3.tasteBud.object.Recipe;
+
 public class SearchProcessor {
+
+
+    public static List<Recipe> searchResults(String text)
+    {
+        List<Recipe> results = RecipeStub.getStoredRecipes();
+
+        results = searchName(results,text);
+
+        return results;
+    }
+
+    private static List<Recipe> searchName(List<Recipe> list, String text)
+    {
+        List<Recipe> searchResults = new ArrayList<>();
+
+
+        String[] textSplit = text.split("\\s+");
+        String patternText = "";
+        for(int i = 0; i < textSplit.length; i++)
+        {
+            patternText += "(?=.*" + textSplit[i] + ")";
+
+        }
+
+        patternText = patternText + ".+";
+
+        Pattern pattern = Pattern.compile(patternText, Pattern.CASE_INSENSITIVE);
+
+        Matcher matcher;
+
+        for(Recipe recipe : list)
+        {
+            matcher = pattern.matcher(recipe.getName());
+            if(matcher.find())
+            {
+                searchResults.add(recipe);
+            }
+        }
+        return searchResults;
+    }
+
 
 }
