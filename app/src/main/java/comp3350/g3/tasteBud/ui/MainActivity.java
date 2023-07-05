141f1647b7638945a386a66a681161d1b564fdf4
Bricz <bricz.cruz@gmail.com>
2023-07-04 20:46:57 -0500

    Refactored interface usage to use only one, edited code styles, removed dead code and created singleton for boolean identifying persistence db usage

--- app/src/main/java/comp3350/g3/tasteBud/ui/MainActivity.java
+++ app/src/main/java/comp3350/g3/tasteBud/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package comp3350.g3.tasteBud.ui;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.ImageView;
