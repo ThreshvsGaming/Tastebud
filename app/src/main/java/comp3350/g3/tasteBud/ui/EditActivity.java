1ecf81da3f9c0f471c7ee517b1ea7c85b3c265de
Lyle Arcinas <60361950+lylearcinas@users.noreply.github.com>
2023-07-12 03:30:09 -0500

    Fixed default items not showing up

--- app/src/main/java/comp3350/g3/tasteBud/ui/EditActivity.java
+++ app/src/main/java/comp3350/g3/tasteBud/ui/EditActivity.java
@@ -1,10 +1,13 @@
 package comp3350.g3.tasteBud.ui;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +15,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import comp3350.g3.tasteBud.R;
 import comp3350.g3.tasteBud.logic.PersistenceSingleton;
@@ -29,12 +40,14 @@ public class EditActivity extends FragmentActivity {
     TextView previewRecipeDescription;
     TextView previewRecipeTags;
     TextView previewRecipeIngredients;
+    ImageView previewRecipeImage;
 
     // Stores what the user changes in the page
     String recipeTitle;
     String recipeDescription;
     String recipeTags;
     List<String> recipeIngredients;
+    String recipeImageUri;
 
 
     int recipeId;
@@ -56,7 +69,7 @@ public class EditActivity extends FragmentActivity {
         previewRecipeTags = findViewById(R.id.recipeTags);
         previewRecipeIngredients = findViewById(R.id.recipeIngredients);
         previewRecipeDescription = findViewById(R.id.recipeDescription);
-        previewRecipeUri = f
+        previewRecipeImage = findViewById(R.id.recipeImageDetail);
 
         submitRecipeButton = findViewById(R.id.recipeSubmit);
 
@@ -92,15 +105,78 @@ public class EditActivity extends FragmentActivity {
             handleValidation(validationError);
         });
 
+
+        previewRecipeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                mGetContent.launch(intent);
+            }
+        });
+
+
+        previewRecipeImage.setImageURI(Uri.parse(recipe.getImageUri()));
+
         findViewById(R.id.returnButton).setOnClickListener(v -> finish());
 
     }
+
+    //For selecting, handling and viewing images in Create Activity
+    private final ActivityResultLauncher<Intent> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    try {
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri selectedImage = data.getData();
+                        File saveDirectory = Objects.requireNonNull(this).getFilesDir();
+                        int fileNumber = 0;
+                        File saveFile;
+
+                        // Get InputStream from the Uri
+                        InputStream is = this.getContentResolver().openInputStream(selectedImage);
+
+
+                        //Keep incrementing the image number so that we can have multiple images stored in the local app storage
+                        do {
+                            saveFile = new File(saveDirectory, "recipe_Image" + fileNumber++ + ".jpg");
+                        }
+                        while (saveFile.exists());
+
+
+                        // Copy InputStream to the File
+                        try (OutputStream os = new FileOutputStream(saveFile)) {
+                            byte[] buffer = new byte[1024];
+                            int length;
+                            while ((length = is.read(buffer)) > 0) {
+                                os.write(buffer, 0, length);
+                            }
+                        }
+
+                        // Close the InputStream
+                        is.close();
+
+                        // Get the Uri from the File that we just saved
+                        Uri fileUri = Uri.fromFile(saveFile);
+
+                        // Display the image
+                        previewRecipeImage.setImageURI(fileUri);
+
+                        // Save the Uri as a string
+                        recipeImageUri = fileUri.toString();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
     // Note: refactor when possible, mostly duplicates DetailProcessor
     public void handleValidation(String validationError) {
         if (validationError == null) {
             try {
-                recipeProcessor.updateRecipe(recipeId, recipeTitle, recipeDescription, recipeIngredients,recipeTags, recipeUri);
+                recipeProcessor.updateRecipe(recipeId, recipeTitle, recipeDescription, recipeIngredients,recipeTags, recipeImageUri);
                 setResult(RESULT_OK);
                 finish();
             } catch (IllegalArgumentException e) {
