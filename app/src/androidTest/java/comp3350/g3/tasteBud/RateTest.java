5205fe021e72b6be345305b1bb6c5027bfd98186
ZHIHAO WANG <wangz8@myumanitoba.ca>
2023-07-25 17:33:55 -0500

    Acceptance Tests

---
+++ app/src/androidTest/java/comp3350/g3/tasteBud/RateTest.java
@@ -0,0 +1,66 @@
+package comp3350.g3.tasteBud;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Tap;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import comp3350.g3.tasteBud.ui.MainActivity;
+
+
+@RunWith(AndroidJUnit4.class)
+public class RateTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testCreateRecipe() {
+        //move from Main Page to Create Page
+        Espresso.onView(ViewMatchers.withId(R.id.ivCreateButton)).perform(ViewActions.click());
+        // Check if the CreateActivity is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.recipeTitle)).check(matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(ViewMatchers.withId(R.id.recipeTitle)).perform(ViewActions.typeText("Test Recipe"));
+        Espresso.onView(ViewMatchers.withId(R.id.recipeDescription)).perform(ViewActions.typeText("This is Test Recipe"));
+        Espresso.onView(ViewMatchers.withId(R.id.recipeIngredients)).perform(ViewActions.typeText("Test Ingredient"));
+        Espresso.onView(ViewMatchers.withId(R.id.recipeTags)).perform(ViewActions.typeText("Test Tag"));
+
+        // Click on the "Submit" button
+        Espresso.onView(ViewMatchers.withId(R.id.recipeSubmit)).perform(ViewActions.click());
+        Espresso.pressBack();
+        Espresso.pressBack();
+        Espresso.onView(ViewMatchers.withText("Test Recipe"))
+                .perform(ViewActions.click());
+        //Test Rating Level
+
+        Espresso.onView(ViewMatchers.withId(R.id.ratingBar))
+                .perform(ViewActions.click(5,2));
+
+        Espresso.pressBack();
+
+        //Delete Recipe after testing
+        Espresso.onView(withText("Test Recipe")).perform(ViewActions.longClick());
+        Espresso.onView(ViewMatchers.withId(R.id.delete)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withText("YES"))
+                .perform(ViewActions.click());
+
+
+
+
+    }
+
+
+
+}
+
+
